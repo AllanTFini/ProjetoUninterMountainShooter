2474fc3080dd5f2961c68809c73b32d08a2b3ada
AllanTFini <allantfini@gmail.com>
2026-02-18 13:49:41 -0300

    Connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import pygame
+from Game import Game
 
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-
-while True:
-    # CHeck for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Closes window
-            quit()
+game = Game()
+game.run()
