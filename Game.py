d1a5ae00f7171206d4014e5e154bdf7760d294a0
AllanTFini <allantfini@gmail.com>
2026-02-19 11:43:27 -0300

    Created assets folder and finished Menu visual design with background, music and fonts; Created Const file

--- Game.py
+++ Game.py
@@ -1,20 +1,14 @@
 import pygame
+from Const import WINDOW_WIDTH, WINDOW_HEIGHT
 from Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
     
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run() 
             pass
-
-
-            # Check for all events
-            #for event in pygame.event.get():
-                #if event.type == pygame.QUIT:
-                    #pygame.quit() # Closes window
-                    #quit()
