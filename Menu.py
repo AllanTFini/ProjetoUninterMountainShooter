a364ed7a546d8232f35ca0680112bd170ee57914
AllanTFini <allantfini@gmail.com>
2026-02-20 15:32:05 -0300

    Created menu basic interaction; Created abstract class for Entity; Created Entity factory for handling background generation; Added Level1 background parallax

--- Menu.py
+++ Menu.py
@@ -16,21 +16,39 @@ class Menu:
         pygame.mixer.music.load("./asset/Menu.mp3")
         pygame.mixer.music.play(-1)
 
+        menu_option = 0
+
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(60, "Mountain", COLOR_ORANGE, ((WINDOW_WIDTH / 2), 70))
             self.menu_text(60, "Shooter", COLOR_ORANGE, ((WINDOW_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(30, MENU_OPTION[i], COLOR_WHITE, ((WINDOW_WIDTH/2), 200 + 25 * i))
-
-            pygame.display.flip()
+                if i == menu_option:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_YELLOW, ((WINDOW_WIDTH/2), 200 + 25 * i))
+                else:
+                    self.menu_text(30, MENU_OPTION[i], COLOR_WHITE, ((WINDOW_WIDTH/2), 200 + 25 * i))
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit() # Closes window
-                    quit()
+                    quit() #Ends Pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN: # Enter key
+                        return MENU_OPTION[menu_option]
+
+            pygame.display.flip()
 
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
