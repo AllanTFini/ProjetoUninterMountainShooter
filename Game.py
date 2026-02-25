c3fab104af09f3bd7c884c34edd7b945e30a834e
AllanTFini <allantfini@gmail.com>
2026-02-25 14:13:13 -0300

    Began work on score system

--- Game.py
+++ Game.py
@@ -2,6 +2,8 @@ import pygame
 from Const import WINDOW_WIDTH, WINDOW_HEIGHT, MENU_OPTION
 from Level import Level
 from Menu import Menu
+from Score import Score
+
 
 class Game:
     def __init__(self):
@@ -10,6 +12,7 @@ class Game:
     
     def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -20,6 +23,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, "Level2", menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
