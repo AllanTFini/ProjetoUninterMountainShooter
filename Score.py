753646085f130ad0d4e83cd0501878ade254829e
AllanTFini <allantfini@gmail.com>
2026-02-26 12:15:32 -0300

    Added DB with Sqlite3, added score screen with updating scores

--- Score.py
+++ Score.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+import sys
+
 import pygame
-from pygame import Surface
+from pygame import Surface, KEYDOWN
+from pygame.constants import K_RETURN, K_BACKSPACE, K_ESCAPE
 from pygame.font import Font
 from pygame.rect import Rect
 from Const import *
 
 from Const import C_YELLOW
+from DBProxy import DBProxy
 
 
 class Score:
@@ -17,11 +22,41 @@ class Score:
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(self.surf, self.rect)
+        db_proxy = DBProxy('DBScore')
+        name = ''
         while True:
             self.score_text(48, 'YOU WIN!', C_YELLOW, SCORE_POS['Title'])
             if game_mode == MENU_OPTION[0]:
-                text = 'Player 1 enter your name (4 characters):'
+                score = player_score[0]
+                text = 'Enter Player 1 name (4 characters):'
+            if game_mode == MENU_OPTION[1]:
+                score = player_score[0] + player_score[1] / 2
+                text = 'Enter Team name (4 characters):'
+            if game_mode == MENU_OPTION[2]:
+                if player_score[0] >= player_score[1]:
+                    score = player_score[0]
+                    text = 'Enter Player 1 name (4 characters):'
+                else:
+                    score = player_score[1]
+                    text = 'Enter Player 2 name (4 characters):'
+            self.score_text(20, text, C_GREEN, SCORE_POS['EnterName'])
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_RETURN and len(name) == 4:
+                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                        return
+                    elif event.key == K_BACKSPACE:
+                        name = name[:-1]
+                    else:
+                        if len(name) < 4:
+                            name += event.unicode
+
+            self.score_text(20, name, C_GREEN, SCORE_POS['Name'])
 
             pygame.display.flip()
             pass
@@ -30,12 +65,34 @@ class Score:
         pygame.mixer_music.load('./asset/Score.mp3')
         pygame.mixer_music.play(-1)
         self.window.blit(self.surf, self.rect)
+        self.score_text(48, 'TOP 10 SCORE', C_YELLOW, SCORE_POS['Title'])
+        self.score_text(20, 'NAME     SCORE           DATE      ', C_YELLOW, SCORE_POS['Label'])
+        db_proxy = DBProxy('DBScore')
+        list_score = db_proxy.retrieve_top10()
+        db_proxy.close()
+
+        for player_score in list_score:
+            id_, name, score, date = player_score
+            self.score_text(20, f'{name}     {score:05d}     {date}', C_YELLOW,
+                            SCORE_POS[list_score.index(player_score)])
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        return
             pygame.display.flip()
-            pass
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
+
+def get_formatted_date():
+    current_datetime = datetime.now()
+    current_time = current_datetime.strftime("%H:%M")
+    current_date = current_datetime.strftime("%d/%m/%y")
+    return f"{current_time} - {current_date}"
